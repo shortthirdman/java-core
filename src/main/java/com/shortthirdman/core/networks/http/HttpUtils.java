0f17ff6837db8142cf57a5e9437fb399e25b47ab
Swetank Mohanty <shortthirdman@users.noreply.github.com>
2017-06-06 10:17:57 +0530

    [JAVA] Networks - HTTP - Utility classes for HTTP

---
+++ src/main/java/com/shortthirdman/core/networks/http/HttpUtils.java
@@ -0,0 +1,26 @@
+package com.shortthirdman.core.networks.http;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class HttpUtils {
+
+    public static void main(String[] args) {
+        String fromFile = "ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest";
+        String toFile = "F:\\arin.txt";
+
+        try {
+            //connectionTimeout, readTimeout = 10 seconds
+            FileUtils.copyURLToFile(new URL(fromFile), new File(toFile), 10000, 10000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+public static void copyURLToFile(URL source, File destination, int connectionTimeout, int readTimeout) throws IOException;
+*/
---
+++ src/main/java/com/shortthirdman/core/networks/http/HttpUtilsNio.java
@@ -0,0 +1,27 @@
+package com.shortthirdman.core.networks.http;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class HttpUtilsNio {
+
+    public static void main(String[] args) {
+
+        String fromFile = "ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest";
+        String toFile = "F:\\arin.txt";
+
+        try {
+            URL website = new URL(fromFile);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(toFile);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.close();
+            rbc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
